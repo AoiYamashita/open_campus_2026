2e5c121bca8cfe922414673653d9b9fe3076d627
yamashita <yamashitaaoi1230@icloud.com>
2026-07-01 07:34:33 +0000

    make sample code(Talker)

---
+++ src/robot_arm_controll/robot_arm_controll/controll.py
@@ -0,0 +1,20 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import UInt8
+
+class Talker(Node):
+    def __init__(self):
+        super().__init__("talker")
+        self.publisher = self.create_publisher(UInt8,"topic",10)
+        self.timer = self.create_timer(0.5,self.cb)
+        self.number = 0
+    def cb(self):
+        msg = UInt8()
+        msg.data = self.number
+        self.publisher.publish(msg)
+        self.number += 1
+
+def main():
+    rclpy.init()
+    talker = Talker()
+    rclpy.spin(talker)
