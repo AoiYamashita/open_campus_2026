666c7ee9df8f47eae82f0a3caac78dee63909f6b
yamashita <yamashitaaoi1230@icloud.com>
2026-07-01 08:21:48 +0000

    test depend libs

--- src/robot_arm_controll/robot_arm_controll/controll.py
+++ src/robot_arm_controll/robot_arm_controll/controll.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import UInt8
+import Arm_Lib
 
 class Talker(Node):
     def __init__(self):
--- src/robot_arm_controll/setup.py
+++ src/robot_arm_controll/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools','Arm_lib','smbus'],
     zip_safe=True,
     maintainer='root',
     maintainer_email='yamashitaaoi1230@icloud.com',
