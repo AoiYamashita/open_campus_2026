9e5e1535a2efb196380d482cd5e949884e766b87
yamashita <yamashitaaoi1230@icloud.com>
2026-07-02 22:54:11 +0900

    add launch

---
+++ src/robot_arm_controll/launch/sample.launch.py
@@ -0,0 +1,15 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    controller = launch_ros.actions.Node(
+            package = "robot_arm_controll",
+            executable = "controll",
+            output = "screen"
+            )
+
+    return launch.LaunchDescription([])
