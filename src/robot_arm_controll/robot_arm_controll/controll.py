64e4cdd67612ca57dc0992885aabe7cbfe7a6e80
yamashita <yamashitaaoi1230@icloud.com>
2026-07-03 02:10:30 +0900

    make controll from app

--- src/robot_arm_controll/robot_arm_controll/controll.py
+++ src/robot_arm_controll/robot_arm_controll/controll.py
@@ -3,20 +3,30 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import UInt8
 from std_msgs.msg import Int32MultiArray
+from std_msgs.msg import Float64MultiArray
 
+import numpy as np
 from . import Arm_Lib
 import time
+
+R1_L = 82.85
+R2_L = 82.85
+R3_L = 79.05
+HAND = 50.0
 
 class Controller(Node):
     def __init__(self):
         super().__init__("controller")
         self.servo_pub = self.create_publisher(Int32MultiArray,"arm_args",10)
         self.servo_sub = self.create_subscription(Int32MultiArray,"arm_order",self.app_order,1)
+        self.hand_pub = self.create_publisher(Float64MultiArray,"hand_pos",10)
+        self.hand_sub = self.create_subscription(Float64MultiArray,"hand_order",self.hand_pos_order,10)
 
         self.timer = self.create_timer(0.1,self.cb)
 
         # self.arm = Arm_Lib.Arm_Device()
 
+        self.servo_deg_arr = [0,0,0,0,0,0]
         self.number = 0
 
         # angle = [90,90,90,90,90,30]
@@ -24,15 +34,29 @@ class Controller(Node):
         #     self.arm.Arm_serial_servo_write(i,angle[i-1],10000)
         #     time.sleep(0.02)
     def cb(self):
-        # servo_arg_arr = []
+        deg_arr = []
         # for i in range(1,7):
-        #     arg = self.arm.Arm_serial_servo_read(i)
-        #     servo_arg_arr.append(arg)
+        #     deg = self.arm.Arm_serial_servo_read(i)
+        #     deg_arr.append(deg)
         msg = Int32MultiArray()
-        # msg.data = servo_arg_arr
-        msg.data = [self.number]*6
+        # msg.data = deg_arr
+        msg.data = [90,0,90,23,102,40]
+        deg_arr = [90,0,90,23,102,40]
         self.servo_pub.publish(msg)
 
+        self.servo_deg_arr = deg_arr.copy()
+
+        msg = Float64MultiArray()
+        args = [np.pi*i/180.0 for i in self.servo_deg_arr]
+        R = R1_L*np.cos(args[1])+R2_L*np.cos(args[1]+args[2])+(R3_L+HAND)*np.cos(args[1]+args[2]+args[3])
+        Y = R1_L*np.sin(args[1])+R2_L*np.sin(args[1]+args[2])+(R3_L+HAND)*np.sin(args[1]+args[2]+args[3])
+        X = R*np.cos(args[0])
+        Z = R*np.sin(args[0])
+        msg.data = [X,Y,Z,\
+                    self.servo_deg_arr[1]+self.servo_deg_arr[2]+self.servo_deg_arr[3],\
+                    self.servo_deg_arr[4],\
+                    self.servo_deg_arr[5]]
+        self.hand_pub.publish(msg)
         self.number += 1
     def app_order(self,msg):
         arr = msg.data
@@ -40,7 +64,41 @@ class Controller(Node):
         # for id,i in enumerate(arr):
         #     self.arm.Arm_serial_servo_write(id,i,servo_time)
         #     time.sleep(0.02)
-        # time.sleep(servo_time/1000.0)
+        # time.sleep(np.max([0.05,servo_time/1000.0]))
+
+    def hand_pos_order(self,msg):
+        arr = msg.data
+        x = arr[0]
+        y = arr[1]
+        z = arr[2]
+        entry_arg = arr[3]
+        hand_arg = arr[4]
+        hand_open = arr[5]
+
+        args = self.servo_deg_arr.copy()
+        args[0] = int(180*np.arctan2(z,x)/np.pi)
+        dx = (R3_L+HAND)*np.cos(np.pi*entry_arg/180)
+        dy = (R3_L+HAND)*np.sin(np.pi*entry_arg/180)
+        x2 = x - dx
+        y2 = y - dy
+        theta1 = np.arccos((x2**2+y2**2+R1_L**2-R2_L**2)/(2*R1_L*np.sqrt(x2**2+y2**2)))\
+                +np.arctan(y2/x2)
+        theta2 = np.arctan((y2-R1_L*np.sin(theta1))/(x2-R1_L*np.cos(theta1)))-theta1
+
+        args[1] = int(180*theta1/np.pi)
+        args[2] = int(180*theta2/np.pi)
+        args[3] = entry_arg
+        args[4] = hand_arg
+        args[5] = hand_open
+
+        max_ddeg = np.max(abs(np.array(args)-np.array(self.servo_deg_arr)))
+
+        # servo_time = max_ddeg*100 # ms
+        # for id,i in enumerate(args):
+        #     self.arm.Arm_serial_servo_write(id,i,servo_time)
+        #     time.sleep(0.02)
+        # time.sleep(np.max([0.05,servo_time/1000.0]))
+
 
 def main():
     rclpy.init()
--- src/robot_arm_controll/robot_arm_controll/controll_app.py
+++ src/robot_arm_controll/robot_arm_controll/controll_app.py
@@ -2,11 +2,13 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import UInt8
 from std_msgs.msg import Int32MultiArray
+from std_msgs.msg import Float64MultiArray
+
+import numpy as np
 from . import Arm_Lib
 import time
 
 import tkinter as tk
-
 
 class controll_app(Node):
     def __init__(self):
@@ -14,14 +16,19 @@ class controll_app(Node):
         self.servo_sub = self.create_subscription(Int32MultiArray,"arm_args",self.servo_args,1)
         self.servo_pub = self.create_publisher(Int32MultiArray,"arm_order",10)
         
+        self.hand_pub = self.create_publisher(Float64MultiArray,"hand_order",10)
+        self.hand_sub = self.create_subscription(Float64MultiArray,"hand_pos",self.hand_posision,10)
+
         self.servo_args_arr = [0]*6
         self.number = 0
+        self.reset_flag = True
+        self.reset_pos_flag = True
+        self.coordinate = [0,0,0,0,0,0] # x,y,z,entry deg,hand deg,open close
         
         self.root = tk.Tk()
         self.root.title("Jetson GUI Controller")
         self.root.geometry("600x450")
 
-        # --- TEXT LABEL ---
         self.text_label = tk.Label(
             self.root,
             text="-"*10,
@@ -32,19 +39,31 @@ class controll_app(Node):
         )
         self.text_label.pack(pady=10,anchor="w")
 
+        self.hand_pos_label = tk.Label(
+            self.root,
+            text="-"*10,
+            font=("Arial", 12, "bold"),
+            padx=1,          # Horizontal padding
+            pady=5,           # Vertical padding
+            relief=tk.RIDGE, bd=5,
+        )
+        self.hand_pos_label.pack(pady=10,anchor="w")
+
+        servo_roles = ["right-left","R1","R2","R3","hand","open close"]
+
         self.scales = [tk.Scale(
             self.root,
             from_=0,
             to=180,
             orient=tk.HORIZONTAL,
-            width=30,
+            width=25,
             length=350,
-            label=f"Servo {i+1}",
+            label=f"Servo {i+1} ( {servo_roles[i]} )",
             command=self.scale_change
         ) for i in range(6)]
         
         for num,i in enumerate(self.scales):
-            i.place(x=200, y=num*65+20)
+            i.place(x=200, y=num*70+5)
 
         self.root.bind("<KeyPress>", self.on_key_press)
         self.root.after(10, self.spin_ros)
@@ -52,25 +71,57 @@ class controll_app(Node):
         rclpy.spin_once(self, timeout_sec=0.0)
         self.root.after(10, self.spin_ros)
 
-    def on_key_press(self,event):
-        self.get_logger().info(f"Key pressed: {event.keysym}")
-        if event.keysym == "Return":
-            for num,i in enumerate(self.servo_args_arr):
-                self.scales[num].set(i)
+    def on_key_press(self,event):# key board controll
 
-    def scale_change(self,value):
+        self.get_logger().info(f"Key pressed: {event.keysym}")
+
+        if event.keysym == "w":
+            self.coordinate[2] += 1
+        if event.keysym == "a":
+            self.coordinate[0] -= 1
+        if event.keysym == "s":
+            self.coordinate[2] -= 1
+        if event.keysym == "d":
+            self.coordinate[0] += 1
+        
+        if event.keysym == "q":
+            self.coordinate[1] += 1
+        if event.keysym[0:5] == "e":
+            self.coordinate[1] -= 1
+
+        self.get_logger().info(f"{self.coordinate}")
+        
+        msg = Float64MultiArray()
+        msg.data = self.coordinate
+        self.hand_pub.publish(msg)
+
+    def hand_posision(self,msg):
+        arr = msg.data
+
+        pos_text = "x:{:5.2f}\ry:{:5.2f}\rx:{:5.2f}\r\rEntry Deg:{: 3}\r\rHand Deg:{: 3}\r\rOpen Close:{: 3}".format(arr[0],arr[1],arr[2],arr[3],arr[4],arr[5])
+        self.hand_pos_label.config(text=pos_text)
+
+        if self.reset_pos_flag:
+            self.coordinate = arr
+        self.reset_pos_flag = False
+
+    def scale_change(self,value):# scale data pub
         arr = [i.get() for i in self.scales]
         msg = Int32MultiArray()
         msg.data = arr
         self.servo_pub.publish(msg)
 
-    def servo_args(self,msg):
+    def servo_args(self,msg):# text value,scale reset
         self.servo_args_arr = msg.data
         text_msg = ""
         for num,i in enumerate(self.servo_args_arr):
             text_msg += f"id{num+1} : {i}\n"
         self.text_label.config(text=text_msg[:-1])
         
+        if self.reset_flag:
+            for num,i in enumerate(self.servo_args_arr):
+                self.scales[num].set(i)
+        self.reset_flag = False
         
 
 
