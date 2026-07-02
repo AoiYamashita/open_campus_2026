737e42063111c963cfc0c0730152cd3e03307d19
yamashita <yamashitaaoi1230@icloud.com>
2026-07-02 06:38:53 +0000

    make sample code

--- src/robot_arm_controll/robot_arm_controll/controll.py
+++ src/robot_arm_controll/robot_arm_controll/controll.py
@@ -1,21 +1,26 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import UInt8
-import Arm_Lib
+from Arm_Lib import Arm_Device
 
-class Talker(Node):
+class Controller(Node):
     def __init__(self):
-        super().__init__("talker")
-        self.publisher = self.create_publisher(UInt8,"topic",10)
-        self.timer = self.create_timer(0.5,self.cb)
+        super().__init__("controller")
+        self.arm = Arm_Device()
+        # self.publisher = self.create_publisher(UInt8,"topic",10)
+        self.timer = self.create_timer(3.0,self.cb)
         self.number = 0
     def cb(self):
-        msg = UInt8()
-        msg.data = self.number
-        self.publisher.publish(msg)
+        # msg = UInt8()
+        # msg.data = self.number
+        # self.publisher.publish(msg)
+        if self.number%2 == 0:
+            self.arm.Arm_serial_servo_write(6, 90, 500)
+        else:
+            self.arm.Arm_serial_servo_write(6, 30, 500)
         self.number += 1
 
 def main():
     rclpy.init()
-    talker = Talker()
-    rclpy.spin(talker)
+    cont = Controller()
+    rclpy.spin(cont)
